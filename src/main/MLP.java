f94ae5b647d42152d035d2e54fe497dcf66970c1
LuizLangkammer <luizeduardo112001@gmail.com>
2022-09-06 15:33:27 -0300

    Staring MLP, MLPRunner, FileReader

--- src/main/MLP.java
+++ src/main/MLP.java
@@ -5,34 +5,110 @@ import java.util.Random;
 public class MLP {
 
     private double ni;
-    private int numIn,numOut,numH;
-    private int qntOut;
+    private int numInput,numOutput,numIntermediary;
+
     private double[][] wh;
     private double[][] wo;
 
-    public MLP(double ni, int numIn, int numOut, int numH){
+    public MLP(double ni, int numInput, int numOutput, int numIntermediary){
 
         this.ni = ni;
-        this.numIn = numIn;
-        this.numOut = numOut;
-        this.numH = numH;
+        this.numInput = numInput;
+        this.numOutput = numOutput;
+        this.numIntermediary = numIntermediary;
 
         Random random = new Random();
 
-        wh = new double[numIn+1][numH];
-        wo = new double[numH+1][numOut];
+        wh = new double[numInput+1][numIntermediary];
+        wo = new double[numIntermediary+1][numOutput];
 
-        for(int i=0;i<numIn;i++) {
-            for(int j=0;j<numH;j++) {
-                wh[i][j] = (random.nextDouble(0.6)-0.3);
+        for(int i=0;i<numInput+1;i++) {
+            for(int j=0;j<numIntermediary;j++) {
+                wh[i][j] =(random.nextDouble(0.6)-0.3);
             }
         }
-        for(int i=0;i<numH;i++) {
-            for(int j=0;j<numOut;j++) {
+        for(int i=0;i<numIntermediary+1;i++) {
+            for(int j=0;j<numOutput;j++) {
                 wo[i][j] = (random.nextDouble(0.6)-0.3);
             }
         }
+    }
 
+    public double[] learn(double[] input, double[] desiredOutput){
+
+        double[] entries = new double[input.length + 1];
+        for(int i=0;i<input.length;i++) {
+            entries[i]=input[i];
+        }
+        entries[input.length] = 1;
+
+        double[] h = sum(numIntermediary, entries, wh, true);
+        double[] o = sum(numOutput, h, wo, false);
+
+        double [] deltaO = new double[numOutput];
+        for(int i=0; i<numOutput; i++){
+            deltaO[i] = o[i] * (1-o[i]) * (desiredOutput[i] - o[i]);
+        }
+
+        double[] deltaH = new double[numIntermediary];
+
+        for(int i=0; i<numIntermediary; i++){
+            double soma = 0;
+            for(int j=0; j<numOutput; j++){
+                soma += deltaO[j] * wo[i][j];
+            }
+            deltaH[i] = h[i] * (1-h[i]) * soma;
+        }
+
+        for(int i=0; i<numInput+1; i++){
+            for(int j=0; j<numIntermediary; j++){
+                wh[i][j] += (ni * deltaH[j] * entries[i]);
+            }
+        }
+
+        for(int i=0; i<numIntermediary+1; i++){
+            for(int j=0; j<numOutput;j++){
+                wo[i][j] += (ni * deltaO[j] * h[i]);
+            }
+        }
+        return o;
+    }
+
+    public double[] exec(double[] input, double[] desiredOutput){
+        double[] entries = new double[input.length + 1];
+        for(int i=0;i<input.length;i++) {
+            entries[i]=input[i];
+        }
+        entries[input.length] = 1;
+
+        double[] h = sum(numIntermediary, entries, wh, true);
+        double[] o = sum(numOutput, h, wo, false);
+        return o;
+    }
+
+    private double[] sum (int qntOut, double[] input, double[][] weight, boolean bias){
+
+        double[] out;
+        if(bias){
+            out = new double[qntOut+1];
+            out[qntOut] = 1;
+        }else{
+            out = new double[qntOut];
+        }
+
+
+        for(int j=0; j<qntOut;j++){
+            for(int i=0; i<input.length; i++){
+                out[j] += input[i] * weight[i][j];
+            }
+            out[j] = sig(out[j]);
+        }
+
+        return out;
+    }
+
+    private double sig(double u) {
+        return 1/(1+Math.exp(-u));
     }
 
 
--- src/main/MLPRunner.java
+++ src/main/MLPRunner.java
@@ -12,16 +12,16 @@ public class MLPRunner {
     public static void main (String[] args) {
 
         //Parameters
-        String basePath = "robo-base.txt";
-        double ni = 0.001;
-        int ages = 100000;
-        int intermediary = 3;
+        String basePath = "xor-base.txt";
+        double ni = 0.3;
+        int ages = 10000;
+        int intermediary = 2;
 
         ArrayList<Sample> base = null;
         try{
             base = new MyInputReader().getBase(basePath);
         }catch(FileNotFoundException e) {
-            System.out.println("Arquivo n�o encontrado");
+            System.out.println("Arquivo nao encontrado");
         }catch(IOException e) {
             System.out.println("Falha na leitura do arquivo");
         }
@@ -32,7 +32,51 @@ public class MLPRunner {
         }
 
         MLP perceptron = new MLP(ni,base.get(0).getInDimension(),base.get(0).getOutDimension(), intermediary);
+        double ageError = 0;
+        double sampleError = 0;
 
+        for(int i=0;i<ages;i++) {
+            ageError=0;
+            for(int j=0; j<base.size();j++) {
+                sampleError=0;
+                double[] output = perceptron.learn(base.get(j).getInput(), base.get(j).getOutput());
+                for(int o=0;o<output.length;o++) {
+                    sampleError+= Math.abs(output[o]-base.get(j).getOutput()[o]);
+                }
+                ageError+=sampleError;
+            }
+            System.out.println("Época "+i+":    "+ageError);
+        }
+
+        int soma=0;
+        for(int j=0; j<base.size();j++) {
+
+            double[] output = perceptron.exec(base.get(j).getInput(), base.get(j).getOutput());
+            for(int i=0;i<base.get(j).getInput().length;i++) {
+                System.out.print(base.get(j).getInput()[i]+"    ");
+            }
+            System.out.print("| ");
+            for(int o=0;o<base.get(j).getOutput().length;o++) {
+                System.out.print(base.get(j).getOutput()[o]+" ");
+            }
+            System.out.print("| ");
+            boolean hit=true;
+            for(int o=0;o<output.length;o++) {
+                System.out.print(output[o]+" ");
+                if(Math.round(output[o])!=base.get(j).getOutput()[o]) {
+                    hit=false;
+                }
+
+            }
+            if(hit) {
+                soma++;
+            }
+            System.out.println("   HIT- " + hit);
+            System.out.println();
+        }
+
+        System.out.println("Porcentagem de acerto: "+((double)soma/base.size())*100+"%");
+        System.out.println("Erro da ultima época:    "+ageError);
     }
 
 
